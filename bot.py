7f2ee5a8317e5366a3f11060abb496df5f0c9a44
arinaleech <ndjdn6062@gmail.com>
2025-01-05 14:35:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,39 +3,29 @@
 # [⚠️ Do not change this repo link ⚠️] :- https://github.com/LISA-KOREA/UPLOADER-BOT-V4
 
 import logging
-import os
-from plugins.config import Config
-from pyrogram import Client as Ntbots
-from pyrogram import filters
-
-# Set up logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-# Set the Pyrogram logging level to WARNING to avoid excessive logs
+import os
+from plugins.config import Config
+
+from pyrogram import Client as Ntbots
+from pyrogram import filters
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-# Main function to start the bot
-if __name__ == "__main__":
-    # Ensure the download location exists
+
+if __name__ == "__main__" :
+
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
-
-    # Set up the plugins directory
     plugins = dict(root="plugins")
-
-    # Initialize the bot client
     Ntbots = Ntbots(
-        "URL UPLOADER BOT",
+        "Url_deploy_bot",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
-        plugins=plugins
-    )
+        plugins=plugins)
 
-    # Print confirmation message
-    print("🎊 I AM ALIVE 🎊  • Support @NT_BOTS_SUPPORT")
-
-    # Start the bot and run it
+    print("🎊 I AM ALIVE 🎊")
     Ntbots.run()
