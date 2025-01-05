1fd503a38987a0a1a5cacbdc4ce7c7e06aaedb06
arinaleech <ndjdn6062@gmail.com>
2025-01-05 19:13:02 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -9,153 +9,113 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-
-import asyncio
+import numpy
 import os
+from PIL import Image
 import time
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-async def place_water_mark(input_file, output_file, water_mark_file):
-    watermarked_file = output_file + ".watermark.png"
-    metadata = extractMetadata(createParser(input_file))
-    width = metadata.get("width")
-    # https://stackoverflow.com/a/34547184/4723940
-    shrink_watermark_file_genertor_command = [
-        "ffmpeg",
-        "-i", water_mark_file,
-        "-y -v quiet",
-        "-vf",
-        "scale={}*0.5:-1".format(width),
-        watermarked_file
-    ]
-    # print(shrink_watermark_file_genertor_command)
-    process = await asyncio.create_subprocess_exec(
-        *shrink_watermark_file_genertor_command,
-        # stdout must a pipe to be accessible as process.stdout
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    t_response = stdout.decode().strip()
-    commands_to_execute = [
-        "ffmpeg",
-        "-i", input_file,
-        "-i", watermarked_file,
-        "-filter_complex",
-        # https://stackoverflow.com/a/16235519
-        # "\"[0:0] scale=400:225 [wm]; [wm][1:0] overlay=305:0 [out]\"",
-        # "-map \"[out]\" -b:v 896k -r 20 -an ",
-        "\"overlay=(main_w-overlay_w):(main_h-overlay_h)\"",
-        # "-vf \"drawtext=text='@FFMovingPictureExpertGroupBOT':x=W-(W/2):y=H-(H/2):fontfile=" + Config.FONT_FILE + ":fontsize=12:fontcolor=white:shadowcolor=black:shadowx=5:shadowy=5\"",
-        output_file
-    ]
-    # print(commands_to_execute)
-    process = await asyncio.create_subprocess_exec(
-        *commands_to_execute,
-        # stdout must a pipe to be accessible as process.stdout
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    t_response = stdout.decode().strip()
-    return output_file
+@pyrogram.Client.on_message(pyrogram.filters.command(["genthumbnail"]))
+async def generate_custom_thumbnail(bot, update):
+    if update.from_user.id in Config.AUTH_USERS:
+        if update.reply_to_message is not None:
+            reply_message = update.reply_to_message
+            if reply_message.media_group_id is not None:
+                download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(reply_message.media_group_id) + "/"
+                save_final_image = download_location + str(round(time.time())) + ".jpg"
+                list_im = os.listdir(download_location)
+                if len(list_im) == 2:
+                    imgs = [ Image.open(download_location + i) for i in list_im ]
+                    inm_aesph = sorted([(numpy.sum(i.size), i.size) for i in imgs])
+                    min_shape = inm_aesph[1][1]
+                    imgs_comb = numpy.hstack(numpy.asarray(i.resize(min_shape)) for i in imgs)
+                    imgs_comb = Image.fromarray(imgs_comb)
+                    # combine: https://stackoverflow.com/a/30228789/4723940
+                    imgs_comb.save(save_final_image)
+                    # send
+                    user = await bot.get_me()
+                    mention = user["mention"]
+                    await bot.send_photo(
+                        chat_id=update.chat.id,
+                        photo=save_final_image,
+                        caption=Translation.CUSTOM_CAPTION_UL_FILE.format(mention),
+                        reply_to_message_id=update.message_id
+                    )
+                else:
+                    await bot.send_message(
+                        chat_id=update.chat.id,
+                        text=Translation.ERR_ONLY_TWO_MEDIA_IN_ALBUM,
+                        reply_to_message_id=update.message_id
+                    )
+                try:
+                    [os.remove(download_location + i) for i in list_im ]
+                    os.remove(download_location)
+                except:
+                    pass
+            else:
+                await bot.send_message(
+                    chat_id=update.chat.id,
+                    text=Translation.REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB,
+                    reply_to_message_id=update.message_id
+                )
+        else:
+            await bot.send_message(
+                chat_id=update.chat.id,
+                text=Translation.REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB,
+                reply_to_message_id=update.message_id
+            )
 
 
-async def take_screen_shot(video_file, output_directory, ttl):
-    # https://stackoverflow.com/a/13891070/4723940
-    out_put_file_name = output_directory + \
-        "/" + str(time.time()) + ".jpg"
-    file_genertor_command = [
-        "ffmpeg",
-        "-ss",
-        str(ttl),
-        "-i",
-        video_file,
-        "-vframes",
-        "1",
-        out_put_file_name
-    ]
-    # width = "90"
-    process = await asyncio.create_subprocess_exec(
-        *file_genertor_command,
-        # stdout must a pipe to be accessible as process.stdout
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    t_response = stdout.decode().strip()
-    if os.path.lexists(out_put_file_name):
-        return out_put_file_name
-    else:
-        return None
-
-# https://github.com/Nekmo/telegram-upload/blob/master/telegram_upload/video.py#L26
-
-async def cult_small_video(video_file, output_directory, start_time, end_time):
-    # https://stackoverflow.com/a/13891070/4723940
-    out_put_file_name = output_directory + \
-        "/" + str(round(time.time())) + ".mp4"
-    file_genertor_command = [
-        "ffmpeg",
-        "-i",
-        video_file,
-        "-ss",
-        start_time,
-        "-to",
-        end_time,
-        "-async",
-        "1",
-        "-strict",
-        "-2",
-        out_put_file_name
-    ]
-    process = await asyncio.create_subprocess_exec(
-        *file_genertor_command,
-        # stdout must a pipe to be accessible as process.stdout
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    t_response = stdout.decode().strip()
-    if os.path.lexists(out_put_file_name):
-        return out_put_file_name
-    else:
-        return None
+@pyrogram.Client.on_message(pyrogram.filters.photo)
+async def save_photo(bot, update):
+    if update.from_user.id in Config.AUTH_USERS:
+        if update.media_group_id is not None:
+            # album is sent
+            download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(update.media_group_id) + "/"
+            # create download directory, if not exist
+            if not os.path.isdir(download_location):
+                os.makedirs(download_location)
+            await bot.download_media(
+                message=update,
+                file_name=download_location
+            )
+        else:
+            # received single photo
+            download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+            await bot.download_media(
+                message=update,
+                file_name=download_location
+            )
+            await bot.send_message(
+                chat_id=update.chat.id,
+                text=Translation.SAVED_CUSTOM_THUMB_NAIL,
+                reply_to_message_id=update.message_id
+            )
 
 
-async def generate_screen_shots(
-    video_file,
-    output_directory,
-    is_watermarkable,
-    wf,
-    min_duration,
-    no_of_photos
-):
-    metadata = extractMetadata(createParser(video_file))
-    duration = 0
-    if metadata is not None:
-        if metadata.has("duration"):
-            duration = metadata.get('duration').seconds
-    if duration > min_duration:
-        images = []
-        ttl_step = duration // no_of_photos
-        current_ttl = ttl_step
-        for looper in range(0, no_of_photos):
-            ss_img = await take_screen_shot(video_file, output_directory, current_ttl)
-            current_ttl = current_ttl + ttl_step
-            if is_watermarkable:
-                ss_img = await place_water_mark(ss_img, output_directory + "/" + str(time.time()) + ".jpg", wf)
-            images.append(ss_img)
-        return images
-    else:
-        return None
+@pyrogram.Client.on_message(pyrogram.filters.command(["delthumbnail"]))
+async def delthumbnail(bot, update):
+    if update.from_user.id in Config.AUTH_USERS:
+        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
+        try:
+            os.remove(download_location + ".jpg")
+            # os.remove(download_location + ".json")
+        except:
+            pass
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL,
+            reply_to_message_id=update.message_id
+        )
