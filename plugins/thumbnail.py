077a781955f380e13bd52f74c09a5c1d5a2c926c
arinaleech <ndjdn6062@gmail.com>
2025-01-06 22:10:47 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -100,7 +100,7 @@ async def save_photo(bot, update):
             )
             await bot.send_message(
                 chat_id=update.chat.id,
-                text=Translation.SAVED_CUSTOM_THUMB_NAIL,
+                text=script.SAVED_CUSTOM_THUMB_NAIL,
                 reply_to_message_id=update.message_id
             )
 
