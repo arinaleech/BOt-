1011f2d1658784b84d4114d49220435e24d4ba5b
arinaleech <ndjdn6062@gmail.com>
2025-01-07 14:38:17 +0530

    Update script.py

--- plugins/script.py
+++ plugins/script.py
@@ -1,136 +1,143 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-class Translation(object):
+class Translation:
+    def __init__(self, lang='en'):
+        # Set the language for translation
+        self.lang = lang
+        self.translations = {
+            'en': {
+                "START_TEXT": """
+👋 Hey {} 
 
-    START_TEXT = """
-👋 Hᴇʏ {} 
+I am a Telegram URL Uploader Bot.
 
-ⵊ Aᴍ Tᴇʟᴇɢʀᴀᴍ URL Uᴘʟᴏᴀᴅᴇʀ Bᴏᴛ.
+**Send me a direct link and I will upload it to Telegram as a file/video**
 
-**Sᴇɴᴅ ᴍᴇ ᴀ ᴅɪʀᴇᴄᴛ ʟɪɴᴋ ᴀɴᴅ ɪ ᴡɪʟʟ ᴜᴘʟᴏᴀᴅ ɪᴛ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴀs ᴀ ꜰɪʟᴇ/ᴠɪᴅᴇᴏ**
+Use the help button to know how to use me.
+                """,
+                "HELP_TEXT": """
+Link to media or file
 
-Usᴇ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴋɴᴏᴡ ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ
+➠ Send a link for upload to Telegram file or media.
 
-"""
-    HELP_TEXT = """
-ʟɪɴᴋ ᴛᴏ ᴍᴇᴅɪᴀ ᴏʀ ꜰɪʟᴇ
+Set thumbnail
 
-➠ sᴇɴᴅ ᴀ ʟɪɴᴋ ꜰᴏʀ ᴜᴘʟᴏᴀᴅ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ꜰɪʟᴇ ᴏʀ ᴍᴇᴅɪᴀ.
+➠ Send a photo to make it as a permanent thumbnail.
 
-sᴇᴛ ᴛʜᴜᴍʙɴᴀɪʟ
+Deleting thumbnail
 
-➠ sᴇɴᴅ ᴀ ᴘʜᴏᴛᴏ ᴛᴏ ᴍᴀᴋᴇ ɪᴛ ᴀs ᴘᴇʀᴍᴀɴᴇɴᴛ ᴛʜᴜᴍʙɴᴀɪʟ.
+➠ Send /delthumb to delete the thumbnail.
 
-ᴅᴇʟᴇᴛɪɴɢ ᴛʜᴜᴍʙɴᴀɪʟ
+Settings
 
-➠ sᴇɴᴅ /delthumb ᴛᴏ ᴅᴇʟᴇᴛᴇ ᴛʜᴜᴍʙɴᴀɪʟ.
+➠ Configure my settings to change upload mode.
 
-sᴇᴛᴛɪɴɢs
+Show Thumbnail
 
-➠ ᴄᴏɴғɪɢᴜʀᴇ ᴍʏ sᴇᴛᴛɪɴɢs ᴛᴏ ᴄʜᴀɴɢᴇ ᴜᴘʟᴏᴀᴅ ᴍᴏᴅᴇ
+➠ Send /showthumb to view custom thumbnail.
+                """,
+                "ABOUT_TEXT": """
+**My Name**: [Uploader Bot](🦋 @bimbo 🦋)
 
-sʜᴏᴡ ᴛʜᴜᴍʙɴᴀɪʟ
+**Database**: [MongoDB](https://cloud.mongodb.com)
 
-➠ sᴇɴᴅ /showthumb ᴛᴏ ᴠɪᴇᴡ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ.
- 
-"""
-    ABOUT_TEXT = """
-**Mʏ ɴᴀᴍᴇ** : [Uᴘʟᴏᴀᴅᴇʀ ʙᴏᴛ ](🦋 @bimbo 🦋)
+**Language**: [Python 3.12.5](https://www.python.org/)
 
-**Dᴀᴛᴀʙᴀsᴇ** : [MᴏɴɢᴏDB](https://cloud.mongodb.com)
+**Framework**: [Pyrogram 2.3.45](https://docs.pyrogram.org/)
 
-**Lᴀɴɢᴜᴀɢᴇ :** [Pʏᴛʜᴏɴ 3.12.5](https://www.python.org/)
+**Developer**: Bimbo69
+                """,
+                "START_BUTTONS": InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton('⚙️ Settings', callback_data='OpenSettings')],
+                        [InlineKeyboardButton('❔ Help', callback_data='help'),
+                         InlineKeyboardButton('👨‍🚒 About', callback_data='about')],
+                        [InlineKeyboardButton('⛔️ Close', callback_data='close')]
+                    ]
+                ),
+                "HELP_BUTTONS": InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton('🏡 Home', callback_data='home'),
+                         InlineKeyboardButton('👨‍🚒 About', callback_data='about')],
+                        [InlineKeyboardButton('⛔️ Close', callback_data='close')]
+                    ]
+                ),
+                "ABOUT_BUTTONS": InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton('🏡 Home', callback_data='home'),
+                         InlineKeyboardButton('❔ Help', callback_data='help')],
+                        [InlineKeyboardButton('⛔️ Close', callback_data='close')]
+                    ]
+                ),
+            },
+            'es': {
+                "START_TEXT": """
+👋 Hola {} 
 
-**Fʀᴀᴍᴇᴡᴏʀᴋ :** [Pʏʀᴏɢᴀᴍ 2.3.45](https://docs.pyrogram.org/)
+Soy un Bot de Cargador de URL de Telegram.
 
-**Dᴇᴠᴇʟᴏᴘᴇʀ :** Bimbo69
-"""
+**Envíame un enlace directo y lo cargaré a Telegram como archivo/video**
 
+Usa el botón de ayuda para saber cómo usarme.
+                """,
+                "HELP_TEXT": """
+Enlace a medios o archivo
 
-    PROGRESS = """
-🏎️ Sᴘᴇᴇᴅ : {3}/s\n\n
-✅ Dᴏɴᴇ : {1}\n\n
-🟰 Tᴏᴛᴀʟ sɪᴢᴇ  : {2}\n\n
-⏳ Tɪᴍᴇ ʟᴇғᴛ : {4}\n\n
-"""
+➠ Envía un enlace para cargar a Telegram archivo o medio.
 
+Configurar miniatura
 
-    START_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('⚙️ sᴇᴛᴛɪɴɢs', callback_data='OpenSettings')
-        ],[
-        InlineKeyboardButton('❔ ʜᴇʟᴘ', callback_data='help'),
-        InlineKeyboardButton('👨‍🚒 ᴀʙᴏᴜᴛ', callback_data='about')
-        ],[
-        InlineKeyboardButton('⛔️ ᴄʟᴏsᴇ', callback_data='close')
-        ]]
-    )
-    HELP_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('🏡 ʜᴏᴍᴇ', callback_data='home'),
-        InlineKeyboardButton('👨‍🚒 ᴀʙᴏᴜᴛ', callback_data='about')
-        ],[
-        InlineKeyboardButton('⛔️ ᴄʟᴏsᴇ', callback_data='close')
-        ]]
-    )
-    ABOUT_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('🏡 ʜᴏᴍᴇ', callback_data='home'),
-        InlineKeyboardButton('❔ ʜᴇʟᴘ', callback_data='help')
-        ],[
-        InlineKeyboardButton('⛔️ ᴄʟᴏsᴇ', callback_data='close')
-        ]]
-    )
-    BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('⛔️ ᴄʟᴏsᴇ', callback_data='close')
-        ]]
-    )
-    TEXT = "sᴇɴᴅ ᴍᴇ ᴀɴʏ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ᴛᴏ sᴇᴛ ɪᴛ"
-    IFLONG_FILE_NAME = " Only 64 characters can be named . "
-    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
-    ABS_TEXT = " Please don't be selfish."
-    FORMAT_SELECTION = "Nᴏᴡ Sᴇʟᴇᴄᴛ Tʜᴇ Dᴇsɪʀᴇᴅ Fᴏʀᴍᴀᴛ ᴏʀ Fɪʟᴇ 🗄️ Sɪᴢᴇ ᴛᴏ Uᴘʟᴏᴀᴅ"
-    SET_CUSTOM_USERNAME_PASSWORD = """"""
-    NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    DOWNLOAD_START = "Dᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛᴏ ᴍʏ sᴇʀᴠᴇʀ ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ  ⏳"
-    UPLOAD_START = "📤 Uᴘʟᴏᴀᴅɪɴɢ Pʟᴇᴀsᴇ Wᴀɪᴛ"
-    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB. use this bot @LinkToFileUploaderBot"
-    #AFTER_SUCCESSFUL_UPLOAD_MSG = " OWNER : Lisa 💕\nFor the List of Telegram Bots"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Dᴏᴡɴʟᴏᴀᴅᴇᴅ ɪɴ {} sᴇᴄᴏɴᴅs.\n\nTʜᴀɴᴋs Fᴏʀ Usɪɴɢ Mᴇ\n\nUᴘʟᴏᴀᴅᴇᴅ ɪɴ {} sᴇᴄᴏɴᴅs"
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
-    SAVED_CUSTOM_THUMB_NAIL = "Cᴜsᴛᴏᴍ ᴠɪᴅᴇᴏ / ғɪʟᴇ ᴛʜᴜᴍʙɴᴀɪʟ sᴀᴠᴇᴅ. Tʜɪs ɪᴍᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴜsᴇᴅ ɪɴ ᴛʜᴇ ᴠɪᴅᴇᴏ / ғɪʟᴇ."
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Cᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ᴄʟᴇᴀʀᴇᴅ sᴜᴄᴄᴇsғᴜʟʟʏ"
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = " "
-    DOWNLOAD_FAILED = "🔴 Eʀʀᴏʀ 🔴"
-    NO_CUSTOM_THUMB_NAIL_FOUND = "Nᴏ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ғᴏᴜɴᴅ"
-    NO_VOID_FORMAT_FOUND = "ERROR... <code>{}</code>"
-    FILE_NOT_FOUND = "Error, File not Found!!"
-    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    SOMETHING_WRONG = "<code>Something Wrong. Try again.</code>"
-    FF_MPEG_RO_BOT_RE_SURRECT_ED = """Syntax: /trim HH:MM:SS for screenshot of that specific time."""
-    FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First send /downloadmedia to any media so that it can be downloaded to my local. \nSend /storageinfo to know the media, that is currently downloaded."
-    FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
-    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "A saved media already exists. Please send /storageinfo to know the current media details."
-    USER_DELETED_FROM_DB = "User <a href='tg://user?id={}'>{}</a> deleted from DataBase."
-    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "Reply to a Telegram media (MKV), to extract embedded streams"
-    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
-    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
-    INVALID_UPLOAD_BOT_URL_FORMAT = "URL format is incorrect. make sure your url starts with either http:// or https://. You can set custom file name using the format link | file_name.extension"
-    ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
-    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "Join : @TGBotsCollectionbot \n For the list of Telegram bots. "
-    EXTRACT_ZIP_INTRO_ONE = "Send a compressed file first, Then reply /unzip command to the file."
-    EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
-    UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
-    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
-    CANCEL_STR = "Process Cancelled"
-    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    FORCE_SUBSCRIBE_TEXT = "<code>Sorry Dear You Must Join My Updates Channel for using me 😌😉....</code>"
-    BANNED_USER_TEXT = "<code>You are Banned!</code>"
-    CHECK_LINK = "Pʀᴏᴄᴇssɪɴɢ ʏᴏᴜʀ ʟɪɴᴋ ⌛"
+➠ Envía una foto para configurarla como miniatura permanente.
 
+Eliminar miniatura
+
+➠ Envia /delthumb para eliminar la miniatura.
+
+Configuración
+
+➠ Configura mis ajustes para cambiar el modo de carga.
+
+Mostrar miniatura
+
+➠ Envía /showthumb para ver la miniatura personalizada.
+                """,
+                "ABOUT_TEXT": """
+**Mi Nombre**: [Uploader Bot](🦋 @bimbo 🦋)
+
+**Base de datos**: [MongoDB](https://cloud.mongodb.com)
+
+**Lenguaje**: [Python 3.12.5](https://www.python.org/)
+
+**Framework**: [Pyrogram 2.3.45](https://docs.pyrogram.org/)
+
+**Desarrollador**: Bimbo69
+                """,
+                "START_BUTTONS": InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton('⚙️ Configuración', callback_data='OpenSettings')],
+                        [InlineKeyboardButton('❔ Ayuda', callback_data='help'),
+                         InlineKeyboardButton('👨‍🚒 Acerca de', callback_data='about')],
+                        [InlineKeyboardButton('⛔️ Cerrar', callback_data='close')]
+                    ]
+                ),
+                "HELP_BUTTONS": InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton('🏡 Inicio', callback_data='home'),
+                         InlineKeyboardButton('👨‍🚒 Acerca de', callback_data='about')],
+                        [InlineKeyboardButton('⛔️ Cerrar', callback_data='close')]
+                    ]
+                ),
+                "ABOUT_BUTTONS": InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton('🏡 Inicio', callback_data='home'),
+                         InlineKeyboardButton('❔ Ayuda', callback_data='help')],
+                        [InlineKeyboardButton('⛔️ Cerrar', callback_data='close')]
+                    ]
+                ),
+            }
+        }
+
+    def get_translation(self, key):
+        """Fetch the translation for the current language."""
+        return self.translations[self.lang].get(key, key)
 
